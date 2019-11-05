fa02e82ca6277b07f733c70951804ec938e18c56
surunhua <278639349@qq.com>
2019-11-05 23:26:19 +0800

    add chess

--- src/components/chess.vue
+++ src/components/chess.vue
@@ -4,50 +4,21 @@
     <span>筛选条件：</span>
     <span>卡费：</span>
     <select v-model="charge">
-    <option value="#"></option>
-    <option value="1">1费</option>
-    <option value="2">2费</option>
-    <option value="3">3费</option>
-    <option value="4">4费</option>
-    <option value="5">5费</option>
+      <option :value="chargeList.indexOf(k)" v-for="k in chargeList">{{k}}<span v-if="k>0">费</span></option>
   </select>
     <span>种族：</span>
     <select v-model="race_id">
-      <option value="#"></option>
-      <option value="1">恶魔</option>
-      <option value="2">龙</option>
-      <option value="3">浪人</option>
-      <option value="4">极地</option>
-      <option value="5">帝国</option>
-      <option value="6">忍者</option>
-      <option value="7">贵族</option>
-      <option value="8">暗影</option>
-      <option value="9">海盗</option>
-      <option value="10">机器人</option>
-      <option value="11">虚空</option>
-      <option value="12">约德尔人</option>
-      <option value="13">海克斯科技</option>
-      <option value="14">狂野</option>
+      <option :value="raceList.indexOf(race)" v-for="race in raceList">{{race}}</option>
     </select>
     <span>职业：</span>
     <select v-model="vocation_id">
-      <option value="#"></option>
-      <option value="1">剑士</option>
-      <option value="2">斗士</option>
-      <option value="3">元素使</option>
-      <option value="4">护卫</option>
-      <option value="5">枪手</option>
-      <option value="6">骑士</option>
-      <option value="7">游侠</option>
-      <option value="8">换形师</option>
-      <option value="9">法师</option>
-      <option value="10">刺客</option>
-
-
+      <option :value="vocationList.indexOf(vocation)" v-for="vocation in vocationList">{{vocation}}</option>
     </select>
     <div v-if="race_id>0">种族羁绊：{{this.$store.state.raceList[race_id].name}}:{{this.$store.state.raceList[race_id].intro}}</div>
     <div v-if="vocation_id>0">职业羁绊：{{this.$store.state.vocationList[vocation_id].name}}：{{this.$store.state.vocationList[vocation_id].intro}}</div>
-
+    <div class="chess_search container">
+        <div class="nuo">{{dataSave[1].name}}</div>
+    </div>
 
   </div>
 </template>
@@ -59,14 +30,27 @@
         return {
           race_id:0,
           vocation_id:0,
-          charge:0
+          charge:0,
+          raceList:['','恶魔','龙','浪人','极地','帝国','忍者','贵族','暗影','海盗','虚空','约德尔人','海克斯科技','狂野'],
+          vocationList:['','剑士','斗士','元素使','护卫','枪手','骑士','游侠','换形师','法师','刺客'],
+          chargeList:['',1,2,3,4,5],
+          dataSave:this.$store.state.chess_data
 
         }
       }
     }
-    console.log(this);
 </script>
 
 <style scoped>
+    .container {
+      margin-top: 20px;
+      width: 400px;
+      margin: 0 auto;
+    }
+    .nuo{
 
+      height: 100px;
+      color: white;
+      background: url("../assets/chess/诺克萨斯之手.jpg")
+    }
 </style>
