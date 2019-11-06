5321c21a752414803ef79c1f79c9de5f629601b5
surunhua <278639349@qq.com>
2019-11-06 23:05:25 +0800

    hello

--- src/components/chess.vue
+++ src/components/chess.vue
@@ -14,10 +14,23 @@
     <select v-model="vocation_id">
       <option :value="vocationList.indexOf(vocation)" v-for="vocation in vocationList">{{vocation}}</option>
     </select>
-    <div v-if="race_id>0">种族羁绊：{{this.$store.state.raceList[race_id].name}}:{{this.$store.state.raceList[race_id].intro}}</div>
-    <div v-if="vocation_id>0">职业羁绊：{{this.$store.state.vocationList[vocation_id].name}}：{{this.$store.state.vocationList[vocation_id].intro}}</div>
+    <div id="raceAndVocationIntro">
+      <div v-if="race_id>0" ><b>种族羁绊</b>：{{this.$store.state.raceList[race_id].name}}:{{this.$store.state.raceList[race_id].intro}}</div>
+      <div v-if="vocation_id>0"><b>职业羁绊</b>：{{this.$store.state.vocationList[vocation_id].name}}：{{this.$store.state.vocationList[vocation_id].intro}}</div>
+    </div>
     <div class="chess_search container">
-        <div class="nuo">{{dataSave[1].name}}</div>
+        <div class="chess_card"
+             v-if=" (charge==chess.charge || charge==0)&&
+                    (raceList[race_id]==chess.race||raceList[race_id]=='')&&
+                    (vocationList[vocation_id]==chess.vocation ||vocationList[vocation_id]=='')"
+             v-for="chess in chess_data">
+          <img :src='"../../static/img/"+chess.name+".jpg"' width="100%">
+          {{chess.name}}<br/>
+          种族：{{chess.race}}<br>
+          职业：{{chess.vocation}}<br>
+          费用：${{chess.charge}}<br/>
+          技能：{{chess.skill}}<br>
+        </div>
     </div>
 
   </div>
@@ -31,26 +44,36 @@
           race_id:0,
           vocation_id:0,
           charge:0,
-          raceList:['','恶魔','龙','浪人','极地','帝国','忍者','贵族','暗影','海盗','虚空','约德尔人','海克斯科技','狂野'],
-          vocationList:['','剑士','斗士','元素使','护卫','枪手','骑士','游侠','换形师','法师','刺客'],
+          raceList:['','剧毒','炼狱','林地','海洋','沙漠','钢铁','电能','山脉','风','光','暗','水晶'],
+          vocationList:['','剑士','炼金师','守护神','掠食者','狂战士','秘术','游侠','德鲁伊','法师','刺客','化身','召唤使'],
           chargeList:['',1,2,3,4,5],
-          dataSave:this.$store.state.chess_data
-
+          chess_data:this.$store.state.chess_data,
+          url:'../../static/img/chess.jpg'
         }
-      }
+      },
+
+
     }
 </script>
 
-<style scoped>
+<style>
     .container {
-      margin-top: 20px;
       width: 400px;
+      height: 1000px;
       margin: 0 auto;
+      margin-top: 30px;
     }
-    .nuo{
-
-      height: 100px;
-      color: white;
-      background: url("../assets/chess/诺克萨斯之手.jpg")
+    #raceAndVocationIntro {
+      width: 500px;
+      margin: 0 auto;
+      text-align: left;
     }
+  .chess_card{
+    text-align: left;
+    margin-top: 10px;
+    border: 1px solid black;
+    padding: 10px;
+    background-color: lightgreen;
+    color: black;
+  }
 </style>
